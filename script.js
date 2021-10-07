fd0ab5a872cbf4c5acd7306d7a2e346ae9a8e21a
Tharindu Yalegama <87438005+tharinduyalegamalucky@users.noreply.github.com>
2021-10-06 21:13:09 -0700

    second commit

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const musicContainer = document.querySelector('.music-container')
 const platBtn = document.querySelector('#play')
 const prevBtn = document.querySelector('#prev')
 const nextBtn = document.querySelector('#next')
-const audio = document.querySelector('#audio');
-const progress = document.querySelector('.progress');
-const progressContainer = document.querySelector('.progress-container');
-const title = document.querySelector('#title');
-const cover = document.querySelector('#cover');
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
 
 //Song titles
 const songs = ['Anatha maruthe', 'Me diganthaye', 'Sanda pana wage']
